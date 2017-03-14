211ad2cd2444842ed9b0bec57b8e2b183a362a0a
Bryce Benson <bryce@graphiti.com>
2017-03-14 09:40:23 -0700

    changed asset output

--- gulp/revision.js
+++ gulp/revision.js
@@ -6,8 +6,8 @@ var path = require('path');
 gulp.task('revision', ['styles','scripts', 'images', 'svg'], function() {
     return gulp.src(['staging/css/*.css', 'staging/js/*.js', 'staging/img/*.*', 'staging/svg/*.svg'], {base: path.join(process.cwd(), 'staging')})
         .pipe(rev())
-        .pipe(gulp.dest('static'))
+        .pipe(gulp.dest('static/assets'))
         .pipe(rev.manifest())
-        .pipe(del({dest: 'static'}))       
-        .pipe(gulp.dest('static'));
+        .pipe(del({dest: 'static/assets'}))
+        .pipe(gulp.dest('static/assets'));
 });
